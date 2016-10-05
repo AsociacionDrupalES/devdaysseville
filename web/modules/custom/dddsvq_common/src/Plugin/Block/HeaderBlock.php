716e00e8d330c74ee86d76e52a65f63ea876ded2
jlbellido <pose16@gmail.com>
2016-10-05 20:39:24 +0200

    #7: Remove unused services.

--- web/modules/custom/dddsvq_common/src/Plugin/Block/HeaderBlock.php
+++ web/modules/custom/dddsvq_common/src/Plugin/Block/HeaderBlock.php
@@ -15,44 +15,8 @@ use Drupal\Core\Render\Renderer;
  *  admin_label = @Translation("Header block"),
  * )
  */
-class HeaderBlock extends BlockBase implements ContainerFactoryPluginInterface {
+class HeaderBlock extends BlockBase {
 
-  /**
-   * Drupal\Core\Render\Renderer definition.
-   *
-   * @var \Drupal\Core\Render\Renderer
-   */
-  protected $renderer;
-  /**
-   * Construct.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param string $plugin_definition
-   *   The plugin implementation definition.
-   */
-  public function __construct(
-        array $configuration,
-        $plugin_id,
-        $plugin_definition,
-        Renderer $renderer
-  ) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->renderer = $renderer;
-  }
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('renderer')
-    );
-  }
   /**
    * {@inheritdoc}
    */
