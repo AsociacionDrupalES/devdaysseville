e5d8087d2c26c5ebec38b355fa0b86656d736834
ckrina <cristina@ymbra.com>
2017-01-02 19:38:38 +0100

    #68 Home styles: Added theming for home News section.

--- web/modules/custom/dddsvq_home/js/fullPage_home.js
+++ web/modules/custom/dddsvq_home/js/fullPage_home.js
@@ -1,10 +1,19 @@
-(function ($, Drupal) {
+/**
+ * @file
+ * Custom Javascript that starts the fullPage.js library.
+ */
+
+ (function ($, Drupal) {
 
   Drupal.behaviors.fullPage_home = {
     attach: function (context, settings) {
-      $('#fullPage').fullpage(
-        { responsiveWidth: 960,
+      $('#fullPage').fullpage({
+          responsiveWidth: 960,
           responsiveHeight: 700,
+          resize: false,
+          autoScrolling: false,
+          fitToSection: false,
+          bigSectionsDestination: 'top',
         }
       );
 
