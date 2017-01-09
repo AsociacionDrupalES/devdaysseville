cbf9e765613a3a66806b7bd8bab786f285dc7fbc
jlbellido <pose16@gmail.com>
2017-01-09 20:49:17 +0100

    #76: Define the theme_wrappers, templates and common main sponsors block

---
+++ web/modules/custom/dddsvq_sponsors/src/Plugin/Block/MainSponsorsBlock.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\dddsvq_sponsors\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'MainSponsorsBlock' block.
+ *
+ * @Block(
+ *  id = "main_sponsors_block",
+ *  admin_label = @Translation("Main sponsors block"),
+ * )
+ */
+class MainSponsorsBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $platinum_sponsors = [
+      '#type' => 'view',
+      '#name' => 'sponsors',
+      '#display_id' => 'block_1',
+      '#arguments' => [],
+    ];
+
+    $gold_sponsors = [
+      '#type' => 'view',
+      '#name' => 'sponsors',
+      '#display_id' => 'block_2',
+      '#arguments' => [],
+    ];
+
+    $build = [
+      '#theme_wrappers' => ['sponsors_wrapper'],
+      'platinum' => [
+        '#theme' => 'sponsors',
+        '#title' => 'Platinum sponsors',
+        '#sponsor_type' => 'platinum',
+        '#sponsors' => $platinum_sponsors,
+      ],
+      'gold' => [
+        '#theme' => 'sponsors',
+        '#title' => 'Gold sponsors',
+        '#sponsor_type' => 'gold',
+        '#sponsors' => $gold_sponsors,
+      ]
+    ];
+
+    return $build;
+  }
+}
