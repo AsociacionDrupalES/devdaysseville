334875ad60bdeec84225ebf936c16d408563b097
María del Carmen García Peral <mcgarcia@MacBook-Pro-de-Maria.local>
2017-03-01 18:39:07 +0100

    #251: Program page styles

--- web/themes/da_vinci/js/da-vinci.js
+++ web/themes/da_vinci/js/da-vinci.js
@@ -6,6 +6,12 @@
 (function ($) {
   Drupal.behaviors.da_vinciTheme = {
     attach: function (context) {
+
+$('.node--type-schedule-page .field--name-field-session > .field__item').each(function() {
+  if ($(this).length==2) {
+    $(this).parent().addClass('multiple');
+  }
+});
       
     }
   };
